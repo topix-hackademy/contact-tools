d16f7c504d10d2620260893d72b2080c80fa13f0
alexcomu <alex.comunian@gmail.com>
2016-07-11 17:09:55 +0200

    service model added

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 ## My Models
-from .models import CompanyType
+from .models import CompanyType, Service
 
 class CompanyTypeAdmin(admin.ModelAdmin):
 
@@ -14,3 +14,17 @@ class CompanyTypeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(CompanyType, CompanyTypeAdmin)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Service Info', {'fields': ['service_name', 'is_valid']}),
+        ('Token Management', {'fields': ['token']}),
+        ('Notes', {'fields': ['notes']})
+    ]
+    list_display = ('service_name', 'token','is_valid', 'creation_date', 'activation_date', 'delete_date')
+    list_filter = ['creation_date', 'is_valid']
+    search_fields = ['service_name']
+
+admin.site.register(Service, ServiceAdmin)
+
--- contacts/models.py
+++ contacts/models.py
@@ -3,12 +3,31 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 import datetime
 
+def create_uid():
+    import uuid
+    return str(uuid.uuid4())
+
 @python_2_unicode_compatible
 class CompanyType(models.Model):
 
     type_name = models.CharField('Company Type', max_length=200)
     is_valid = models.BooleanField('Is Valid', default=True)
-    creation_date = models.DateTimeField('Date Created', default=datetime.datetime.now)
+    creation_date = models.DateTimeField('Creation Date', default=datetime.datetime.now)
 
     def __str__(self):
         return self.type_name
+
+
+@python_2_unicode_compatible
+class Service(models.Model):
+
+    service_name = models.CharField('Service Name', max_length=200)
+    token = models.CharField('Token', default=create_uid, max_length=200)
+    is_valid = models.BooleanField('Is Valid', default=True)
+    creation_date = models.DateTimeField('Creation Date', default=datetime.datetime.now)
+    activation_date = models.DateTimeField('Activation Date', default=datetime.datetime.now)
+    delete_date = models.DateTimeField('Delete Date', null=True)
+    notes = models.TextField('Notes', blank=True)
+
+    def __str__(self):
+        return self.service_name + " - " + self.token
