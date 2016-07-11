330d31f34d257f24858cab8798db59c9ba307dee
alexcomu <alex.comunian@gmail.com>
2016-07-11 17:35:45 +0200

    change delete_date on change model

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+import datetime
 
 ## My Models
 from .models import CompanyType, Service
+
+### COMPANY TYPE ADMIN
 
 class CompanyTypeAdmin(admin.ModelAdmin):
 
@@ -15,6 +18,7 @@ class CompanyTypeAdmin(admin.ModelAdmin):
 
 admin.site.register(CompanyType, CompanyTypeAdmin)
 
+### SERVICE ADMIN
 
 class ServiceAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -25,6 +29,20 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display = ('service_name', 'token','is_valid', 'creation_date', 'activation_date', 'delete_date')
     list_filter = ['creation_date', 'is_valid']
     search_fields = ['service_name']
+
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            old_obj = Service.objects.get(id=obj.id)
+            if old_obj.is_valid and not obj.is_valid:
+                # set service not valid
+                obj.delete_date = datetime.datetime.now()
+                obj.token = "INVALID"
+            elif not old_obj.is_valid and obj.is_valid:
+                # set service valid
+                obj.delete_date = None
+                obj.activation_date = datetime.datetime.now()
+        obj.save()
 
 admin.site.register(Service, ServiceAdmin)
 
