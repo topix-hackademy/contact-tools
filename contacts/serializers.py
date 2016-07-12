2f9ca3ed38b901d802ad46604a515f1edf4cbc57
beccaris <andreabeccaris88@gmail.com>
2016-07-12 18:04:31 +0200

    change serializer to let api populatete relationships

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -5,11 +5,15 @@ from .models import Company, CompanyType, Service, Contact, ContactType, CCRelat
 class CompanyTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyType
-        fields = ('type_name', 'is_valid', 'creation_date')
+        fields = ('id','type_name', 'is_valid', 'creation_date')
+
 
 class CompanySerializer(serializers.ModelSerializer):
+
+    company_type = CompanyTypeSerializer(many=True, read_only=True)
+
     class Meta:
         model = Company
-        fields = ('company_custom_id', 'company_name', 'company_short_name', 'company_business_name', 'company_vat_number',
+        fields = ('id','company_custom_id', 'company_name', 'company_short_name', 'company_business_name', 'company_vat_number',
                   'company_tax_code', 'company_address','company_cap','company_city','company_province','company_country',
-                  'company_phone_number','company_fax','company_website','company_notes','creation_date','company_type')
+                'company_phone_number', 'company_fax', 'company_website', 'company_notes', 'creation_date', 'company_type')
