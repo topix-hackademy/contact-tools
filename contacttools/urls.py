65d1c3a5aeb4ea7d413ba8304d32c3c86b455361
alexcomu <alex.comunian@gmail.com>
2016-07-13 12:52:19 +0200

    added some style

--- contacttools/urls.py
+++ contacttools/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='Index'),
     url(r'^api/', include('contacts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
