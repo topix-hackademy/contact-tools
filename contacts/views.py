e9bfcdb308cbf3eb3de34891dce89ca2182168c0
alexcomu <alex.comunian@gmail.com>
2016-07-13 10:31:00 +0200

    logger

--- contacts/views.py
+++ contacts/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
 def index(request):
     return HttpResponse("Control is an illusion")
 
--- contacttools/local_settings.py
+++ contacttools/local_settings.py
@@ -121,3 +121,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+### LOGGING MANAGEMENT
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/contacttools.log',
+        }
+    },
+    'loggers': {
+        'contacttools': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
+
+
+
+
