84c7e5aef3cd6652adb5ece1ff44d824ca3d24ca
beccaris <andreabeccaris88@gmail.com>
2016-07-15 11:27:13 +0200

    working on API for role and company type

--- contacts/serializers_company.py
+++ contacts/serializers_company.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Company, CompanyType, Contact
 
+
 class CompanyTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyType
